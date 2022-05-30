1bbca555c8fe561b1c762355cafd3be1ac0953a5
Park, Jonghyeon <belline0124@gmail.com>
2022-05-30 16:31:02 +0900

    Update imagewrapper styling

--- assets/js/mainCover.js
+++ assets/js/mainCover.js
@@ -9,6 +9,18 @@ var coverImages = [
   {
     localPath: null,
     remotePath: 'https://www.utophotel.com/UtopUnity_common/images/homepage/company/marina_03.jpg'
+  },
+  {
+    localPath: null,
+    remotePath: 'https://www.utophotel.com/UtopUnity_common/images/homepage/company/marina_04.jpg'
+  },
+  {
+    localPath: null,
+    remotePath: 'https://www.utophotel.com/UtopUnity_common/images/homepage/company/marina_05.jpg'
+  },
+  {
+    localPath: null,
+    remotePath: 'https://www.hotelscombined.co.kr/himg/97/2f/ad/expediav2-5269182-47ac79-571756.jpg'
   }
 ];
 
